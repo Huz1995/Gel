6a24dcb8b984e432da95059485ea14d0b0921cb3
Hari <haripatel130@gmail.com>
2021-06-14 19:58:56 +0100

    added defintiion to gel, added font themeing too,, chose poppins

--- lib/screens/front_page.dart
+++ lib/screens/front_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
 
 import '../widgets/register_button.dart';
 import '../widgets/login_button.dart';
 import '../widgets/gel_and_skip_row.dart';
+import '../widgets/gel_definition.dart';
 
 class FrontPage extends StatelessWidget {
   @override
@@ -16,39 +16,7 @@ class FrontPage extends StatelessWidget {
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.35,
             ),
-            Container(
-              padding: EdgeInsets.all(5),
-              width: MediaQuery.of(context).size.width * .80,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    child: Text(
-                      "gel",
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
-                    margin: EdgeInsets.only(
-                      bottom: 10,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 60,
-                    child: RotateAnimatedTextKit(
-                        repeatForever: true,
-                        duration: const Duration(milliseconds: 5000),
-                        text: [
-                          "verb: \ntake a definite form or begin to work well.",
-                          "noun: \nis used to harden hair into a particular hairstyle.",
-                        ],
-                        textStyle: Theme.of(context).textTheme.bodyText2,
-                        textAlign: TextAlign.start,
-                        alignment: AlignmentDirectional
-                            .topStart // or Alignment.topLeft
-                        ),
-                  ),
-                ],
-              ),
-            ),
+            GelDefinition(),
             SizedBox(
               height: 25,
             ),
---
+++ lib/widgets/gel_definition.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class GelDefinition extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      width: MediaQuery.of(context).size.width * .80,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Text(
+              "gel",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            margin: EdgeInsets.only(
+              bottom: 10,
+            ),
+          ),
+          SizedBox(
+            height: 60,
+            child: RotateAnimatedTextKit(
+                repeatForever: true,
+                duration: const Duration(milliseconds: 5000),
+                text: [
+                  "verb: \ntake a definite form or begin to work well.",
+                  "noun: \nis used to harden hair into a particular hairstyle.",
+                ],
+                textStyle: Theme.of(context).textTheme.bodyText2,
+                textAlign: TextAlign.start,
+                alignment: AlignmentDirectional.topStart // or Alignment.topLeft
+                ),
+          ),
+        ],
+      ),
+    );
+  }
+}
