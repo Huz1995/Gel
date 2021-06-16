00e7764a5cb1d3179803eb65908fd5ab78c4a03e
Hari <haripatel130@gmail.com>
2021-06-16 09:07:40 +0100

    adding google maps to the applicaiton, needed to set a google cloud account and then create api kets and implement those keys within the flutter ios and android files

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCUd-52bUysS9lOA3t-gwaHQI38sX82KBQ")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/screens/unauth_map_page.dart
+++ lib/screens/unauth_map_page.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatelessWidget {
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Map"),
       ),
-      body: Text("Map"),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        myLocationButtonEnabled: true,
+        initialCameraPosition: _kGooglePlex,
+      ),
     );
   }
 }
