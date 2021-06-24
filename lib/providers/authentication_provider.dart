8d1d51232fa567a9aaf177e0bf0c3eaca7439e9c
Hari <haripatel130@gmail.com>
2021-06-24 12:20:00 +0100

    linking app to firebase ios and androoid side and dowloaing dependecies

--- lib/providers/authentication_provider.dart
+++ lib/providers/authentication_provider.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthenticationProvider with ChangeNotifier {}
+class AuthenticationProvider with ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //sign in with email
+
+  //register with email
+
+  //sing out
+
+}
